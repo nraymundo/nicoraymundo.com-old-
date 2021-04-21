ded142d27e9c398ce2a547e96a6c8c8f690ad83e
Nicolas Raymundo <nicoraymundo2@gmail.com>
2021-04-20 20:32:58 -0700

    small updates to journal dict

--- src/Journal.js
+++ src/Journal.js
@@ -6,29 +6,29 @@ import Entry from './components/JournalEntry'
 
 const entries = [
   {
-    'place': 'LONDON & EDINBURGH',
-    'date': '03.2020',
-    'images': imgs[0]
+    place: 'LONDON & EDINBURGH',
+    date: '03.2020',
+    images: imgs[0]
   },
   {
-    'place': 'SAN FRANCISCO',
-    'date': '01.2020',
-    'images': imgs[1]
+    place: 'SAN FRANCISCO',
+    date: '01.2020',
+    images: imgs[1]
   },
   {
-    'place': 'LOS ANGELES',
-    'date': '01.2020',
-    'images': imgs[2]
+    place: 'LOS ANGELES',
+    date: '01.2020',
+    images: imgs[2]
   },
   {
-    'place': 'DAY N VEGAS',
-    'date': '11.2019',
-    'images': imgs[3]
+    place: 'DAY N VEGAS',
+    date: '11.2019',
+    images: imgs[3]
   },
   {
-    'place': 'JAPAN',
-    'date': '05.2019',
-    'images': imgs[4]
+    place: 'JAPAN',
+    date: '05.2019',
+    images: imgs[4]
   }
 ]
 
