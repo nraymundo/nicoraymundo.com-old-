0d7f9bb1757e865ab14f39e3439ae13542d27df3
Nico Raymundo <nicoraymundo2@gmail.com>
2023-05-26 20:28:20 -0700

    refactor(home): update info

--- src/Info.js
+++ src/Info.js
@@ -6,10 +6,10 @@ export default function Info() {
   return (
     <div className="main-paragraph">
       <p className="intro-content">
-        Nico Raymundo is a developer and photographer from the San Francisco Bay
-        Area, currently at{" "}
-        <a class="intext-hyperlink" href="http://lmu.edu" target="_blank">
-          Loyola Marymount University
+        Nico Raymundo is a developer and casual photographer from the San Francisco Bay
+        Area, currently in Los Angeles and at{" "}
+        <a class="intext-hyperlink" href="http://americanexpress.com" target="_blank">
+          American Express
         </a>
         .
       </p>
@@ -18,42 +18,52 @@ export default function Info() {
         <div className="about">
           <p>ABOUT</p>
           <p>
-            I am currently a senior studying Computer Science at Loyola Marymount
-            University in Los Angeles, CA. I was born and raised in San Jose, CA
-            but decided to migrate south to further my education. Living in Los
-            Angeles has given me an opportunity to try new things, visit new
-            unfamiliar places, and cultivate relationships with incredible
-            people from around the globe.
+            I am currently working remotely as a software engineer for American
+            Express. I was born and raised in San Jose, CA but decided to migrate
+            south for college. Post-grad, I decided to stay in southern california,
+            setting up residence in Los Angeles. Living here has given me an
+            opportunity to try new things, visit new unfamiliar places, and
+            cultivate relationships with incredible people from around the globe.
           </p>
           <p>
-            At LMU, I've had the opportunity to take part in a multitude of
-            extra-curricular activities. As the current Vice-President of
+            At American Express, my main responsibility is creating, maintaining
+            and enhancing various web apps built with ReactJS. In addition to this,
+            my role includes contributing to code reviews and helping build out fully
+            automated testing for our applications.
+          </p>
+          <p>
+            At LMU, I had the opportunity to take part in a multitude of
+            extra-curricular activities. As the former Vice-President of
             Outreach for the LMU chapter of the Association for Computing
-            Machinery, we as an e-board have worked to hold fun, engaging events
-            for the LMU CS community, with our biggest event being the first
-            ever LMU Hackathon. I am also an active member of Delta Sigma Phi, a
-            social fraternity on campus, as well as a teaching assistant for the
-            Computer Science department.
+            Machinery, we were tasked with organizing events for the LMU CS
+            community, with our biggest event being the first ever LMU Hackathon.
           </p>
           <p>
             Outside of school, I enjoy playing sports with friends, taking photos around the city,
             listening to {" "}
-            <a class="about-intext-hyperlink" href="https://music.apple.com/profile/nicoraymundo" target="_blank">
+            <a class="about-intext-hyperlink" href="https://open.spotify.com/user/kdotoverlord?si=b932916d03cf4b81" target="_blank">
               music
             </a>
-            , working on my side projects, or going on runs around my neighborhood with my dog.
-            I'd love to get to know you, so don't hesitate to get in touch!
+            , working on my side projects, or going on runs and exploring the area around me.
           </p>
+          <p>I'd love to get to know you, so don't hesitate to get in touch!</p>
         </div>
         <div className="side-section">
           <div className="cv">
             <p>CV</p>
             <p>
+              American Express
+              <br />
+              Engineer III
+              <br />
+              2021 - Present
+            </p>
+            <p>
               Loyola Marymount University
               <br />
               BA Computer Science
               <br />
-              2017 - Present
+              2017 - 2021
             </p>
             <p>
               Maxim Integrated
@@ -67,14 +77,14 @@ export default function Info() {
               <br />
               Computer Science Teaching Assistant
               <br />
-              2018 - Present
+              2018 - 2021
             </p>
             <p>
               Association for Computing Machinery
               <br />
               Vice President of Outreach
               <br />
-              2018 - Present
+              2018 - 2021
             </p>
           </div>
           <div className="connect">
